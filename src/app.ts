ed4bb9e6fec2c7b154dd1bd47444d257c5318e12
jayb2302 <jonbja01@easv365.dk>
2025-02-17 09:32:06 +0100

    ✨ feat(database): add database connection and update app configuration
    - Add database connection
    - Modify app.ts to integrate database connection and middleware

--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,22 @@ import express, { Application, Request, Response } from 'express';
 import dotenvFlow from 'dotenv-flow';
 
 import routes from './routes';
+import { testConnection, connect } from './repository/database';
 
 dotenvFlow.config();
 
 // Create Express Application
 const app: Application = express();
 
-app.use('/api', routes);
 
 // Middleware
 export function startServer() {
+    app.use(express.json());
+    // Bind routes to the app 
+    app.use('/api', routes);
+    
+    testConnection();
+    // connect();
     const PORT = process.env.PORT || 4000;
     app.listen(PORT, function() {
         console.log(`Server is running on port ${PORT}`);
