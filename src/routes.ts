e033fd4a5b82df4f6313ed337b848cdae73092c4
jayb2302 <jonbja01@easv365.dk>
2025-02-22 14:18:28 +0100

    ✨feat(auth): add authController with register and login functions and secure product CRUD routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response } from "express";
-import { createProduct, deleteProductById, getAllProduct, getProductById, updateProductById } from "./controllers/productController";
+import { createProduct, deleteProductById, getAllProducts, getProductById, updateProductById, getProductsByQuery } from "./controllers/productController";
+import { loginUser, registerUser, verifyToken } from "./controllers/authController";
 
 const router: Router = Router();
 
@@ -12,10 +13,20 @@ router.get("/", (req: Request, res: Response) => {
     res.status(200).send("Welcome to the MENTS API");
 });
 
-router.post("/products", createProduct);
-router.get("/products", getAllProduct);
+// auth
+router.post('/user/register', registerUser);
+router.post('/user/login', loginUser);
+
+// Create a new product
+router.post("/products", verifyToken, createProduct);
+
+// Get all products
+router.get("/products", getAllProducts);
 router.get("/products/:id", getProductById);
-router.put("/products/:id", updateProductById);
-router.delete("/products/:id", deleteProductById);
+router.get("/products/query/:key/:val", getProductsByQuery);
+
+// Update and Delete a product
+router.put("/products/:id", verifyToken, updateProductById);
+router.delete("/products/:id", verifyToken,  deleteProductById);
 
 export default router;
