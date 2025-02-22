3b1e39b640c01ba7707972d0804233df275c5efa
jayb2302 <jonbja01@easv365.dk>
2025-02-22 18:49:20 +0100

    🛠️ chore(cors): add auth-token to allowed headers

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ app.use(
   cors({
     origin: 'http://localhost:5173', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'auth-token'],
   })
 );
 
