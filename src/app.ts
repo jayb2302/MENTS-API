5f7cef136101e7e7181de4cbd373dc1523c6a6e5
jayb2302 <jonbja01@easv365.dk>
2025-02-22 12:56:09 +0100

    🛠️ chore(cors): Add CORS configuration to allow frontend requests

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express, { Application, Request, Response } from 'express';
 import dotenvFlow from 'dotenv-flow';
-
+import cors from 'cors';
 import routes from './routes';
 import { testConnection, connect } from './repository/database';
 
@@ -9,17 +9,27 @@ dotenvFlow.config();
 // Create Express Application
 const app: Application = express();
 
+// Enable CORS for all requests
+app.use(
+  cors({
+    origin: 'http://localhost:5173', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
+
+app.use(cors());
 
 // Middleware
 export function startServer() {
-    app.use(express.json());
-    // Bind routes to the app 
-    app.use('/api', routes);
-    
-    testConnection();
-    // connect();
-    const PORT = process.env.PORT || 4000;
-    app.listen(PORT, function() {
-        console.log(`Server is running on port ${PORT}`);
-    });
+  app.use(express.json());
+  // Bind routes to the app
+  app.use('/api', routes);
+
+  testConnection();
+  // connect();
+  const PORT = process.env.PORT || 4000;
+  app.listen(PORT, function () {
+    console.log(`Server is running on port ${PORT}`);
+  });
 }
